90061c63ad360883e3a7c9a8d67ad9af792eff3a
Malshatnawi <mohammed.i.alshatnawi@gmail.com>
2021-02-16 14:22:49 +0200

    committed project#4 php_mysql

---
+++ deletefromcart.php
@@ -0,0 +1,14 @@
+<?php
+$connection= mysqli_connect("localhost","root" ,"password" ,"muchmoredb");
+
+        if(!$connection){
+            die("Connection failed: " . mysqli_connect_error());
+        }
+session_start();
+$itemtodelete=$_GET['id'];
+
+
+$_SESSION['cartqty']=array_diff_key($_SESSION['cartqty'], [$itemtodelete => ""]);
+// $_SESSION['quantity']=array_diff($_SESSION['quantity'], array($qtytodelete));
+header("location: cart.php");
+?>
